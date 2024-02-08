af6cfe751e56e74caac7e02816c7729a1dd7fd13
FacundoMM <152993838+FacundoMM@users.noreply.github.com>
2024-02-08 16:32:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function likes() {
+    alert('Ninja was liked');
+}
+
+function Log() {
+    document.getElementById('Log').innerHTML = "Logout";
+}
+
+function define() {
+    document.getElementById('defined').remove();
+}
